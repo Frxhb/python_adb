5b7f8f36476c647471c9d4faf4774feaa13e1a84
Frxhb <huber.frani@gmail.com>
2022-02-05 20:00:57 +0100

    edited check_dependencies for pip

--- functions/check_dependencies.py
+++ functions/check_dependencies.py
@@ -1,22 +1,23 @@
+import os
+import subprocess
+import sys
+from packaging import version
+import time
+
+class bcolors:
+        
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def check_all_dependencies():
 
-    import os
-    import subprocess
-    import sys
-    from packaging import version
-    import time
-
-    class bcolors:
-        
-        HEADER = '\033[95m'
-        OKBLUE = '\033[94m'
-        OKCYAN = '\033[96m'
-        OKGREEN = '\033[92m'
-        WARNING = '\033[93m'
-        FAIL = '\033[91m'
-        ENDC = '\033[0m'
-        BOLD = '\033[1m'
-        UNDERLINE = '\033[4m'
 
     print (bcolors.WARNING + "Going to check your dependencies...\n" + bcolors.ENDC)
     print (bcolors.OKBLUE + "1.  ppab-module\n" + bcolors.ENDC)
@@ -39,7 +40,7 @@ def check_all_dependencies():
                     subprocess.check_call([sys.executable, "-m", "pip", "install", package_ppadb])
                 install(package_ppadb) 
             else:
-                print("Okay!Close program now.\n")
+                print("Okay! Close program now.\n")
                 exit()
 
     ppadb_dependecy()
@@ -58,10 +59,55 @@ def check_all_dependencies():
         print (bcolors.WARNING + sliced_sys_ver + bcolors.ENDC +"\n")
 
         if version.parse(sliced_sys_ver) >= version.parse(destination_ver):
-            print (bcolors.OKGREEN + "[✓]Your python version seems to be compatible...\nMoving on..." + bcolors.ENDC)
+            print (bcolors.OKGREEN + "[✓]Your python version seems to be compatible...\n" + bcolors.ENDC)
         else:
             print (bcolors.FAIL + "[x]Your installed version is not compatible..." + bcolors.ENDC)
             ask_user_update_python = input ("You want to install the compatible python version (3.8.x)?")
             if ask_user_update_python in ["Yes", "y", "Y", "yes"]:
                 os.system("sudo apt install python3")
+            else:
+                print("Okay! Close program now.\n")
+                exit()
+
     python_dependency()
+    time.sleep(1)
+
+
+    print (bcolors.OKBLUE + "3.  python3-pip\n"+ bcolors.ENDC)
+    time.sleep(1)
+    def pip_dep():
+
+        python_pip_one_location= os.path.exists('/usr/bin/pip')
+        python_pip_two_location= os.path.exists('/usr/bin/pip3')
+
+        if (python_pip_one_location == True) or python_pip_two_location == True:
+            print (bcolors.OKGREEN + "      python3-pip is installed! ✓\n" + bcolors.ENDC)
+        else:
+            print (bcolors.WARNING + "      python3-pip isn't installed! ✕\n" + bcolors.ENDC)
+            ask_user_install_ppadb = input("To move on you need to install the python3-pip.\nYou want to install it now?\nY/n\n>>>")
+            if ask_user_install_ppadb in ['yes', 'Yes', 'Y', 'y']:
+                print("Installing python3-pip...")
+                os.system("sudo apt-get install python3-pip")
+            else:
+                print("Okay! Close program now.\n")
+                exit()
+
+    pip_dep()
+    
+    sys_ver = sys.version
+    sliced_sys_ver = sys_ver[0:7]
+    destination_ver = "3.8"
+    if version.parse(sliced_sys_ver) >= version.parse(destination_ver):
+        print("")
+
+    ppadbfile_one= os.path.exists('/usr/local/lib/python3.8/dist-packages/ppadb')
+    ppadbfile_two=os.path.exists('/home/francesco/.local/lib/python3.8/site-packages/ppadb/')
+
+    python_pip_one_location= os.path.exists('/usr/bin/pip')
+    python_pip_two_location= os.path.exists('/usr/bin/pip3')
+
+    if (ppadbfile_one == True) or (ppadbfile_two == True) and (python_pip_one_location == True) or (python_pip_two_location == True):
+        print (bcolors.OKGREEN + "Everything seems to be fine! We can continue!\n" + bcolors.ENDC)
+        time.sleep(1)
+    else:
+        print(bcolors.FAIL + "Error occured. Please check your dependencies manually:\n1. Python 3.6x+\n2. Python3-pip\n3. pure-python-adb" + bcolors.ENDC)
